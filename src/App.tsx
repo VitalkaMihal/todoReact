cb85af6c0bd5c93ee8a7ccc9efee8e8982916137
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2025-04-23 23:25:08 +0300

    feat(todo): add props

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
 import './App.css'
+import {TodolistItem} from './components/TodolistItem/TodolistItem.tsx'
 
-function App() {
+export const App = () => {
   return (
       <div className="app">
-        <div>
-          <h3>What to learn</h3>
-          <div>
-            <input/>
-            <button>+</button>
-          </div>
-          <ul>
-            <li>
-              <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={true}/> <span>JS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={false}/> <span>React</span>
-            </li>
-          </ul>
-          <div>
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
-          </div>
-        </div>
+        <TodolistItem title="What to learn"/>
+        <TodolistItem title="Songs"/>
+        <TodolistItem title="Books"/>
       </div>
   )
 }
-
-export default App
