4473bc8692b242ee036e896ea54c2debe3386031
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2025-11-13 22:46:13 +0300

    add errorSnackbar

--- src/app/app-slice.ts
+++ src/app/app-slice.ts
@@ -8,6 +8,7 @@ export const appSlice = createSlice({
   initialState: {
     themeMode: "light" as ThemeMode,
     status: "idle" as RequestStatus,
+    error: "null" as string | null,
   },
   reducers: (create) => ({
     changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
@@ -16,15 +17,19 @@ export const appSlice = createSlice({
     setAppStatusAC: create.reducer<{ status: RequestStatus }>((state, action) => {
       state.status = action.payload.status
     }),
+    setAppErrorAC: create.reducer<{ error: string | null }>((state, action) => {
+      state.error = action.payload.error
+    }),
   }),
   selectors: {
     selectThemeMode: (state) => state.themeMode,
     selectStatus: (state) => state.status,
+    selectAppError: (state) => state.error,
   },
 })
 
-export const { changeThemeModeAC, setAppStatusAC } = appSlice.actions
+export const { changeThemeModeAC, setAppStatusAC, setAppErrorAC } = appSlice.actions
 
 export const appReducer = appSlice.reducer
 
-export const { selectThemeMode, selectStatus } = appSlice.selectors
+export const { selectThemeMode, selectStatus, selectAppError } = appSlice.selectors
