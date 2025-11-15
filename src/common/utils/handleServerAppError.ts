e5a07693fe7895106c2d79bbc4b6583f8262d8e5
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2025-11-15 20:05:02 +0300

    add error.ts

--- src/common/utils/handleServerAppError.ts
+++ src/common/utils/handleServerAppError.ts
@@ -3,10 +3,7 @@ import type { BaseResponse } from "@/common/types"
 import type { Dispatch } from "@reduxjs/toolkit"
 
 export const handleServerAppError = <T>(data: BaseResponse<T>, dispatch: Dispatch) => {
-  if (data.messages.length) {
-    dispatch(setAppErrorAC({ error: data.messages[0] }))
-  } else {
-    dispatch(setAppErrorAC({ error: "Some error occurred" }))
-  }
+  const error = data.messages.length ? data.messages[0] : "Some error occurred"
+  dispatch(setAppErrorAC({ error: error }))
   dispatch(setAppStatusAC({ status: "failed" }))
 }
--- src/common/utils/handleServerNetworkError.ts
+++ src/common/utils/handleServerNetworkError.ts
@@ -3,14 +3,12 @@ import type { Dispatch } from "@reduxjs/toolkit"
 import axios from "axios"
 
 export const handleServerNetworkError = (error: unknown, dispatch: Dispatch) => {
-  let errorMessage
+  let errorMessage = "Something went wrong"
 
   if (axios.isAxiosError(error)) {
     errorMessage = error.response?.data?.message || error.message
   } else if (error instanceof Error) {
     errorMessage = `Native error: ${error.message}`
-  } else {
-    errorMessage = JSON.stringify(error)
   }
 
   dispatch(setAppErrorAC({ error: errorMessage }))
