2d15db99d520b637f54c3e187849d758f4167de4
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2025-11-16 18:03:31 +0300

    add Routing

--- src/common/common/routing/Routing.tsx
+++ src/common/common/routing/Routing.tsx
@@ -1,8 +1,15 @@
 import { Main } from "@/app/Main"
+import { Login } from "@/features/features/auth/ui/Login"
 import { Route, Routes } from "react-router"
+
+export const Path = {
+  Main: "/",
+  Login: "login",
+} as const
 
 export const Routing = () => (
   <Routes>
-    <Route path="/" element={<Main />} />
+    <Route path={Path.Main} element={<Main />} />
+    <Route path={Path.Login} element={<Login />} />
   </Routes>
 )
