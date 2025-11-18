4b7f1053775a0376b58a075e2740ee167ab91303
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2025-11-18 22:37:25 +0300

    add interceptor

--- src/features/auth/model/auth-slice.ts
+++ src/features/auth/model/auth-slice.ts
@@ -3,6 +3,7 @@ import { LoginInputs } from "@/features/auth/lib/schemas"
 import { setAppStatusAC } from "@/app/app-slice.ts"
 import { authApi } from "@/features/auth/api/authApi.ts"
 import { ResultCode } from "@/common/enums/enums.ts"
+import { AUTH_TOKEN } from "@/common/constants"
 
 export const authSlice = createAppSlice({
   name: "auth",
@@ -19,6 +20,7 @@ export const authSlice = createAppSlice({
           dispatch(setAppStatusAC({ status: "loading" }))
           const res = await authApi.login(data)
           if (res.data.resultCode === ResultCode.Success) {
+            localStorage.setItem(AUTH_TOKEN, res.data.data.token)
             dispatch(setAppStatusAC({ status: "succeeded" }))
             return { isLoggedIn: true }
           } else {
