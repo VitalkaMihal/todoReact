e72bc67f1ae40503464a8a42d815830564c16423
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2025-11-22 13:23:06 +0300

    add me

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,12 +4,20 @@ import CssBaseline from "@mui/material/CssBaseline"
 import { getTheme } from "@/common/theme/theme.ts"
 import { selectThemeMode } from "@/app/app-slice.ts"
 import { ErrorSnackbar, Header, Routing } from "@/common/components"
-import { useAppSelector } from "@/common/hooks"
+import { useAppDispatch, useAppSelector } from "@/common/hooks"
+import { initializeAppTC } from "@/features/auth/model/auth-slice.ts"
+import { useEffect } from "react"
 
 export const App = () => {
   const themeMode = useAppSelector(selectThemeMode)
 
+  const dispatch = useAppDispatch()
+
   const theme = getTheme(themeMode)
+
+  useEffect(() => {
+    dispatch(initializeAppTC())
+  }, [])
 
   return (
     <ThemeProvider theme={theme}>
