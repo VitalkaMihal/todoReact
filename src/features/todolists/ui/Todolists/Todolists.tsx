60f340e77edd6bf4dcfb737c9ca204a0bf82e273
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2025-11-29 16:26:03 +0300

    add todolistsSkeleton

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -2,9 +2,24 @@ import Grid from "@mui/material/Grid"
 import Paper from "@mui/material/Paper"
 import { TodolistItem } from "@/features/todolists/ui/Todolists/TodolistItem/TodolistItem.tsx"
 import { useGetTodolistsQuery } from "@/features/todolists/api/todolistsApi.ts"
+import { Box } from "@mui/material"
+import { containerSx } from "@/common/styles/container.styles.ts"
+import { TodolistSkeleton } from "@/features/todolists/ui/Todolists/TodolistSkeleton/TodolistSkeleton.tsx"
 
 export const Todolists = () => {
-  const { data: todolists } = useGetTodolistsQuery()
+  const { data: todolists, isLoading } = useGetTodolistsQuery()
+
+  if (isLoading) {
+    return (
+      <Box sx={containerSx} style={{ gap: "32px" }}>
+        {Array(3)
+          .fill(null)
+          .map((_, id) => (
+            <TodolistSkeleton key={id} />
+          ))}
+      </Box>
+    )
+  }
 
   return (
     <>
