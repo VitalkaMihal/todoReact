3fd137a2014f5ce9d8eebc53950f79651e1a69e3
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2025-11-29 17:14:30 +0300

    refactor errors

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,8 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import { AUTH_TOKEN } from "@/common/constants"
-import { setAppErrorAC } from "@/app/app-slice.ts"
-import { isErrorWithMessage } from "@/common/utils/isErrorWithMessage"
-import { ResultCode } from "@/common/enums/enums.ts"
+import { handleError } from "@/common/utils/handleError.ts"
 
 export const baseApi = createApi({
   reducerPath: "todolistsApi",
@@ -18,42 +16,7 @@ export const baseApi = createApi({
       },
     })(args, api, extraOptions)
 
-    let error = "Some error occurred"
-
-    if (result.error) {
-      switch (result.error.status) {
-        case "FETCH_ERROR":
-        case "PARSING_ERROR":
-        case "CUSTOM_ERROR":
-        case "TIMEOUT_ERROR":
-          error = result.error.error
-          break
-        case 403:
-          error = "403 Forbidden Error. Check API-KEY"
-          break
-        case 400:
-          if (isErrorWithMessage(result.error.data)) {
-            error = result.error.data.message
-          } else {
-            error = JSON.stringify(result.error.data)
-          }
-          break
-        default:
-          if (result.error.status >= 500 && result.error.status < 600) {
-            error = "Server error occurred. Please try again later."
-          } else {
-            error = JSON.stringify(result.error)
-          }
-          break
-      }
-      api.dispatch(setAppErrorAC({ error }))
-    }
-
-    if ((result.data as { resultCode: ResultCode }).resultCode === ResultCode.Error) {
-      const messages = (result.data as { messages: string[] }).messages
-      error = messages.length ? messages[0] : error
-      api.dispatch(setAppErrorAC({ error }))
-    }
+    handleError(api, result)
 
     return result
   },
